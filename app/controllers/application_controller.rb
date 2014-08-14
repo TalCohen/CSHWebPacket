c35ebc02a9629d49a030d384d06e5e7c5819dfe5
TalCohen <tcohen@csh.rit.edu>
2014-08-13 22:47:16 -0400

    Removed some old logic from the application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,52 +6,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   force_ssl
 
-=begin
-  before_action do
-    # Find current user
-      ldap = PacketLdap::Ldap.new
-      username = request.env['REMOTE_USER']
-
-      if username == nil # User is accessing from freshmen-packet.csh
-        @freshman_user = true 
-        @freshman_first = true
-        @user = nil
-        
-        # Redirect to freshmen index
-        controller = params[:controller]
-        action = params[:action]
-        if not (controller == "freshmen" and (action == "index" or action == "create"))
-          redirect_to freshmen_path
-        end
-      else # User is accessing from packet.csh
-        @freshman_user = false
-        user = ldap.find_by_username(username)[0]
-        user_uuid = user.entryuuid[0]
-        @user = Upperclassman.find_by uuid: "#{user_uuid}"
-      
-        # Check if user is nil (not onfloor upperclassman)
-        if @user == nil
-          # Create upperclassman as alumni
-          @user = Upperclassman.new
-          @user.create_upperclassman(user, alumni=true)
-        end
-
-        # Check if user is admin
-        admin = File.read("/var/www/priv/packet/admin.txt").chomp
-        if @user.uuid == admin
-          @admin = true
-        end
-      end
-
-      # Instantiate title
-      @title = ""
-  end
-=end
-  ##                 ##
-  ## NEW RESTRUCTURE ##
-  ##                 ##
-
-
   private
 
   def get_uuid

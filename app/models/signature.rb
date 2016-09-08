4f70667964b988a2a3e2befe685f5b27428bee7f
TalCohen <tcohen@csh.rit.edu>
2016-09-07 23:06:44 -0400

    Fixed a constraint bug for uniqueness on signatures.

--- app/models/signature.rb
+++ app/models/signature.rb
@@ -1,5 +1,5 @@
 class Signature < ActiveRecord::Base
     belongs_to :freshman
     belongs_to :signer, polymorphic: true
-    validates :freshman, uniqueness: { scope: :signer }
+    validates :freshman, uniqueness: { scope: [:signer_type, :signer_id] }
 end
